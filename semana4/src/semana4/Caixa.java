0d8e7a4d6488cb3dbf1e26155b1bd492f9c2124b
weverton <wevertonmixalt@gmail.com>
2021-03-31 00:20:15 -0300

    ops o tipo estava bugado em outros lugares

--- semana4/src/semana4/Caixa.java
+++ semana4/src/semana4/Caixa.java
@@ -4,7 +4,7 @@ public class Caixa extends Funcionario {
 
 	private Integer horario;
 
-	public Caixa(String nome, String telefone, String matricula, String salario, Integer horario) {
+	public Caixa(String nome, String telefone, String matricula, Double salario, Integer horario) {
 		super(nome, telefone, matricula, salario);
 		this.horario = horario;
 	}
