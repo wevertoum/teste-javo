55c53c9a208819b7342d79eb790a3229ec7180ea
weverton <wevertonmixalt@gmail.com>
2021-04-06 02:50:42 -0300

    começando a questao 4

---
+++ semana5_4/src/com/tv/ControleRemoto.java
@@ -0,0 +1,16 @@
+package com.tv;
+
+import java.util.Scanner;
+
+public class ControleRemoto {
+
+    public static void main(String[] args) {
+        ControleRemoto controleRemoto = new ControleRemoto();
+        controleRemoto.instanceTv();
+    }
+
+    public void instanceTv() {
+
+
+    }
+}
---
+++ semana5_4/src/com/tv/Televisao.java
@@ -0,0 +1,11 @@
+package com.tv;
+
+public class Televisao {
+    Integer volume;
+    Integer canal;
+
+    Televisao(Integer volume, Integer canal){
+        this.volume = 0;
+        this.canal = 0;
+    }
+}
