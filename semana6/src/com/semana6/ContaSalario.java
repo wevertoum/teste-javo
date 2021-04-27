f0baa568dec56ee9d782dcf3da8f193dccfb0622
weverton <wevertonmixalt@gmail.com>
2021-04-27 02:20:56 -0300

    class and java ContaSalario

--- semana6/src/com/semana6/ContaSalario.java
+++ semana6/src/com/semana6/ContaSalario.java
@@ -1,2 +1,7 @@
-package com.semana6;public class ContaSalario {
+package com.semana6;
+
+public class ContaSalario extends ContaCorrente{
+    ContaSalario(int numero, Cliente dono) {
+        super(numero, dono);
+    }
 }
