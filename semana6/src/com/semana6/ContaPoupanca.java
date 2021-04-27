74e222e67d51eaa4bc285a27fe5c2deb46b25236
weverton <wevertonmixalt@gmail.com>
2021-04-27 02:19:33 -0300

    class and java ContaPoupanca

--- semana6/src/com/semana6/ContaPoupanca.java
+++ semana6/src/com/semana6/ContaPoupanca.java
@@ -1,2 +1,22 @@
-package com.semana6;public class ContaPoupanca {
+package com.semana6;
+
+public class ContaPoupanca extends Conta {
+    ContaPoupanca(int numero, Cliente dono) {
+        super(numero, dono);
+    }
+
+    @Override
+    public void sacar(double valor) {
+        saldo = saldo - (valor + 0.05);
+    }
+
+    public void atualizaSaldo(int percent) {
+        saldo = saldo + (saldo * percent / 100);
+    }
+
+    @Override
+    public void transferir(double valor, Conta destino) {
+        sacar(valor);
+        destino.depositar(valor);
+    }
 }
