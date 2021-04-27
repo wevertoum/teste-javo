5cdaf361010379e363e09a600355fcf8a8c68d0b
weverton <wevertonmixalt@gmail.com>
2021-04-27 02:18:55 -0300

    class and java Cliente

---
+++ semana6/src/com/semana6/Cliente.java
@@ -0,0 +1,26 @@
+package com.semana6;
+
+public class Cliente {
+    private String nome;
+    private String telefone;
+
+    Cliente (String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
