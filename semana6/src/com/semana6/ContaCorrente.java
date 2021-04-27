1e5b6a0eddfc6f699aadc98f1bed33ed8e4a35df
weverton <wevertonmixalt@gmail.com>
2021-04-27 02:19:16 -0300

    class and java ContaCorrente

--- semana6/src/com/semana6/ContaCorrente.java
+++ semana6/src/com/semana6/ContaCorrente.java
@@ -1,2 +1,24 @@
-package com.semana6;public class ContaCorrente {
+package com.semana6;
+
+public class ContaCorrente extends Conta {
+
+    ContaCorrente(int numero, Cliente dono) {
+        super(numero, dono);
+    }
+
+    @Override
+    public void sacar(double valor) {
+        saldo = saldo - (valor + 0.05);
+    }
+
+    @Override
+    public void depositar(double valor) {
+        saldo = saldo + valor;
+    }
+
+    @Override
+    public void transferir(double valor, Conta destino) {
+        sacar(valor);
+        destino.depositar(valor);
+    }
 }
