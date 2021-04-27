2b1dff7b5269329968efa20f02d3224ba2ed38e3
weverton <wevertonmixalt@gmail.com>
2021-04-27 03:43:48 -0300

    uns metodo novo aí

--- semana6/src/com/semana6/ContaSalario.java
+++ semana6/src/com/semana6/ContaSalario.java
@@ -4,4 +4,20 @@ public class ContaSalario extends ContaCorrente{
     ContaSalario(int numero, Cliente dono) {
         super(numero, dono);
     }
+
+    @Override
+    public void sacar(double valor) {
+        saldo = saldo - (valor + 0.01);
+    }
+
+    @Override
+    public void depositar(double valor) {
+        saldo = saldo + (valor - 0.01);
+    }
+
+    @Override
+    public void transferir(double valor, Conta destino) {
+        this.sacar(valor);
+        destino.depositar(valor);
+    }
 }
